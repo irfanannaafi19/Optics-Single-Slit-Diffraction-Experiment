ff1188fd9aa6eba24cba0eba7937d35fce2b720d
Irfan Annaafi <irfanannaafi.2024@student.uny.ac.id>
2026-05-25 17:19:25 +0700

    Update Project_2_Optika.m

--- Project_2_Optika.m
+++ Project_2_Optika.m
@@ -2,33 +2,29 @@ clc;
 clear;
 close all;
 
-% Membaca dan mengambil data warna dari foto
+% Membaca Foto
 I = imread("OPTK_PJ2_SET_006_MXA237.jpg");
 
-% Mengambil warna merah (R) untuk semua kolom dan baris
+% Mengambil warna Merah
 R = I(:,:,1);
 
-% Memotong foto
+% Mengambil Rentang
 for i=1:1:200
     for j=1:1:1000
-        r(i,j) = R(i+170, j+683);  
+        r(i,j) = R(i+170, j+683); 
     end
 end
 
-% Horizontal
+% Mmebuat sumbu vertikal maksimal 1
 Sr=sum(r)/200;
-% plot(Sr)
-
-% Maksimal y = 1 atau maksimal intensitas = 1
 Sn=Sr/max(Sr);
-% plot (Sn)
 
-& Menunjukkan nilai sumbu x sampai nilai negatif
+% Membuat rentang -500 sampai 500
 for j=1:1:1000
     x(j)=j-500;
 end
-% plot(x,Sn)
 
+% Membuat i_teori
 beta=linspace(-2*pi,2*pi,736);
 I_teori=(sin(beta)./beta).^2;
 % plot(beta, I_teori, x, Sn)
@@ -37,9 +33,19 @@ for n=1:1:736
     Sn2(n)=Sn(n+132);
 end
 
+% Mengubah data dimulai dari 0 terhadap sumbu-x
+Sn2=(Sn2-min(Sn2))/(max(Sn2)-min(Sn2));
+
 for m=1:1:736
-    y(m)=(m-361)/(361/(2*pi));
+    y(m)=(m-361)/(371/(2*pi));
 end
 
 % plot(y, Sn2)
-plot(beta, I_teori, y, Sn2)
+% plot(beta, I_teori, y, Sn2)
+
+% MSE
+Se=0;
+for n=1:1:736
+    Se=Se+(I_teori(n)-Sn2(n))^2;
+end
+MSE=Se/736;
